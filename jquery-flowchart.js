72769eb4e1f5968fa9718c18dfae91235b5ab1b2
Neeilan S <neeilans@live.com>
2016-03-30 15:22:42 -0400

    Update jquery-flowchart.js

--- jquery-flowchart.js
+++ jquery-flowchart.js
@@ -4,16 +4,30 @@
 			delay=500;
 		}
 		$(".fc-btn").click(function() {
-		var parent = $(this).parent();
+          var parent = $(this).parent();
         $(".fc-btn").attr("disabled", "true"); //prevent multiple clicks
-  
+
         parent.removeClass(animateIn).addClass(animateOut);
         var nextId = "#C" + $(this).attr("id").substring(1);
+
+        
         setTimeout(function() {
         	parent.hide();
         	$(nextId).addClass(animateIn).show();
         	$(".fc-btn").removeAttr("disabled");
+            $("#fc-home").show();
         }, delay);
     });
-	}
+
+        $("#fc-home").click(function() {
+            $("#fc-home").hide();
+            $(".fc-card").addClass(animateOut);
+            setTimeout(function() {
+                $(".fc-card").hide();
+                $("#Cmain").show();
+                $(".fc-card").removeClass(animateOut).addClass(animateIn);
+                
+            }, delay);
+        })
+    }
 }(jQuery));		
