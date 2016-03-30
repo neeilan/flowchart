8c8b8310d3ad443e4b37095331375052b34f2590
Neeilan S <neeilans@live.com>
2016-03-30 14:22:18 -0400

    rename

--- jquery-flowchart.js
+++ jquery-flowchart.js
@@ -1,18 +1,18 @@
 (function($){
-	$.fn.tree = function (animateIn,animateOut, delay){
+	$.fn.fc = function (animateIn,animateOut, delay){
 		if(!delay || delay<0){
 			delay=500;
 		}
-		$(".tree-btn").click(function() {
+		$(".fc-btn").click(function() {
 		var parent = $(this).parent();
-        $(".tree-btn").attr("disabled", "true"); //prevent multiple clicks
+        $(".fc-btn").attr("disabled", "true"); //prevent multiple clicks
   
         parent.removeClass(animateIn).addClass(animateOut);
         var nextId = "#C" + $(this).attr("id").substring(1);
         setTimeout(function() {
         	parent.hide();
         	$(nextId).addClass(animateIn).show();
-        	$(".tree-btn").removeAttr("disabled");
+        	$(".fc-btn").removeAttr("disabled");
         }, delay);
     });
 	}
